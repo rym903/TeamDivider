1d4c8782fddcfa4362c4ffcb887b82312f7f6df3
rym903 <nakagawa.ryoma.84n@gmail.com>
2024-11-28 15:12:24 +0900

    Initial Commit

---
+++ TeamDivider/TeamDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamDividerApp.swift
+//  TeamDivider
+//
+//  Created by nakagawa.ryoma on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct TeamDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeamDividerTests/TeamDividerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamDividerTests.swift
+//  TeamDividerTests
+//
+//  Created by nakagawa.ryoma on 2024/11/28.
+//
+
+import Testing
+@testable import TeamDivider
+
+struct TeamDividerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
